96822697610d1800e51035325f358e2f319e4b3d
devchuks <emmaantisimp@gmail.com>
2026-07-08 13:40:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let payEl = document.getElementById("payment-el");
+
+function payment() {
+  let message = "button clicked";
+  console.log("button clicked");
+  payEl.textContent = "Would you like to purchase our premium service option for $59.99 anually?";
+}
